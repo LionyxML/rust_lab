4f5cb78a53f37505549946201663f1d1d0c70182
Rahul Martim Juliato <rahul.juliato@gmail.com>
2024-03-15 16:45:06 -0300

    feat: simple testing

---
+++ _03_stuff/src/main.rs
@@ -0,0 +1,26 @@
+fn great_common_denominator(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
+#[test]
+fn test_great_common_denominator() {
+    assert_eq!(great_common_denominator(14, 15), 1);
+
+    assert_eq!(
+        great_common_denominator(2 * 3 * 4 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19),
+        3 * 11
+    )
+}
+
+fn main() {
+    println!("{}", great_common_denominator(20, 40));
+}
