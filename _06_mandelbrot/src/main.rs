13dd87b67ba070e26dd89671599874f34b0e5873
LionyxML <rahul.juliato@gmail.com>
2024-03-26 20:32:56 -0300

    feat: mandelbrot partial

---
+++ _06_mandelbrot/src/main.rs
@@ -0,0 +1,65 @@
+use std::str::FromStr;
+
+use num::Complex;
+
+/// Tests if complex is inside Mandelbrot set
+/// If `c` is squared `limit` times and is lesser than 4.0, we
+/// assume it is outside the Mandelbrot set, returning `Some(i)`.
+/// If `c` looks like an member (is limited squared and does not
+/// evaluate > 4) it probably is a member, we return `None`.
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+        z = z * z + c;
+    }
+
+    None
+}
+
+/// Given a type `T` a `s` string and a `separator` char, tests if we have
+/// the format first<T>separator<char>second<T>, returning Some((first, second)). If parsing
+/// fails, returns None
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+            (Ok(l), Ok(r)) => Some((l, r)),
+            _ => None,
+        },
+    }
+}
+
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+     match parse_pair(s, separator) {}
+}
+
+fn main() {
+    todo!()
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("", ','), None);
+    assert_eq!(parse_pair::<i32>("10", ','), None);
+    assert_eq!(parse_pair::<i32>(",10", ','), None);
+    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<f64>("0.5x", ','), None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(
+        parse_complex("1.25,-0.0625"),
+        Some(Complex {
+            re: 1.25,
+            im: -0.0625
+        })
+    );
+    assert_eq!(parse_complex(",-0.0625"), None);
+}
