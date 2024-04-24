37f71393a0a6665d49393de27d9bee0a17b45603
Rahul Martim Juliato <rahul.juliato@gmail.com>
2024-04-24 19:38:02 -0300

    chore: planning

--- Proj_09_axum-postgres-crud-app/src/main.rs
+++ Proj_09_axum-postgres-crud-app/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    routing::{get, patch},
+    Json, Router,
+};
+
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+
+use sqlx::{postgres::PgPoolOptions, PgPool};
+
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    // expose the environment variables
+
+    // set variables from the environment variables
+
+    // create the database pool
+
+    // create our TCP listener
+
+    // compose the routes
+
+    // serve the application
     todo!()
 }
