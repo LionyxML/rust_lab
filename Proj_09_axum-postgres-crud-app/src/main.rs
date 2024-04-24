281bcca8f2d7d6ea7190ccaff12e415314309685
Rahul Martim Juliato <rahul.juliato@gmail.com>
2024-04-24 20:15:01 -0300

    feat: base server and pool connection

--- Proj_09_axum-postgres-crud-app/src/main.rs
+++ Proj_09_axum-postgres-crud-app/src/main.rs
@@ -16,14 +16,37 @@ use tokio::net::TcpListener;
 async fn main() {
     // expose the environment variables
 
+    dotenvy::dotenv().expect("Unable to access .env file");
+
     // set variables from the environment variables
+
+    let server_address = std::env::var("SERVER_ADDRESS").unwrap_or("127.0.0.1:3000".to_owned());
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL not found in the env file");
 
     // create the database pool
 
+    let db_pool = PgPoolOptions::new()
+        .max_connections(16)
+        .connect(&database_url)
+        .await
+        .expect("Can't connect to the database");
+
     // create our TCP listener
+
+    let listener = TcpListener::bind(server_address)
+        .await
+        .expect("Can't init the server");
+
+    println!("Listening on {}", listener.local_addr().unwrap());
 
     // compose the routes
 
+    let app = Router::new().route("/", get(|| async { "Hello World" }));
+
     // serve the application
-    todo!()
+
+    axum::serve(listener, app)
+        .await
+        .expect("Error serving application");
 }
+
