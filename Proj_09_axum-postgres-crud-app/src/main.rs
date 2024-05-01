d83bf69a6b87681af931f0f619a41fb4f75d290f
Rahul Martim Juliato <rahul.juliato@gmail.com>
2024-04-30 21:13:52 -0300

    feat: add routes to get and post tasks / org-mode client

--- Proj_09_axum-postgres-crud-app/src/main.rs
+++ Proj_09_axum-postgres-crud-app/src/main.rs
@@ -21,7 +21,8 @@ async fn main() {
     // set variables from the environment variables
 
     let server_address = std::env::var("SERVER_ADDRESS").unwrap_or("127.0.0.1:3000".to_owned());
-    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL not found in the env file");
+    let database_url =
+        std::env::var("DATABASE_URL").expect("DATABASE_URL not found in the env file");
 
     // create the database pool
 
@@ -41,7 +42,11 @@ async fn main() {
 
     // compose the routes
 
-    let app = Router::new().route("/", get(|| async { "Hello World" }));
+    let app = Router::new()
+        .route("/", get(|| async { "Hello World" }))
+        .route("/tasks", get(get_tasks).post(create_task))
+        .route("/tasks/:task_id", patch(update_task).delete(delete_task))
+        .with_state(db_pool);
 
     // serve the application
 
@@ -50,3 +55,73 @@ async fn main() {
         .expect("Error serving application");
 }
 
+#[derive(Serialize)]
+struct TaskRow {
+    task_id: i32,
+    name: String,
+    priority: Option<i32>,
+}
+
+async fn get_tasks(
+    State(pg_poll): State<PgPool>,
+) -> Result<(StatusCode, String), (StatusCode, String)> {
+    let rows = sqlx::query_as!(TaskRow, "SELECT * FROM tasks ORDER BY task_id")
+        .fetch_all(&pg_poll)
+        .await
+        .map_err(|e| {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                json!({"success": false, "message": e.to_string()}).to_string(),
+            )
+        })?;
+
+    Ok((
+        StatusCode::OK,
+        json!({"success": true, "data": rows}).to_string(),
+    ))
+}
+
+#[derive(Deserialize)]
+struct CreateTaskReg {
+    name: String,
+    priority: Option<i32>,
+}
+
+#[derive(Deserialize, Serialize)]
+struct CreateTaskRow {
+    task_id: i32,
+}
+
+async fn create_task(
+    State(pg_poll): State<PgPool>,
+    Json(task): Json<CreateTaskReg>,
+) -> Result<(StatusCode, String), (StatusCode, String)> {
+    let row = sqlx::query_as!(
+        CreateTaskRow,
+        "INSERT INTO tasks (name, priority) VALUES ($1, $2) RETURNING task_id",
+        task.name,
+        task.priority
+    )
+    .fetch_one(&pg_poll)
+    .await
+    .map_err(|e| {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            json!({"succes": false, "message": e.to_string()}).to_string(),
+        )
+    })?;
+
+    Ok((StatusCode::CREATED, json!({"success": true, "data": row}).to_string()))
+}
+
+async fn update_task(
+    State(pg_poll): State<PgPool>,
+) -> Result<(StatusCode, String), (StatusCode, String)> {
+    todo!();
+}
+
+async fn delete_task(
+    State(pg_poll): State<PgPool>,
+) -> Result<(StatusCode, String), (StatusCode, String)> {
+    todo!();
+}
